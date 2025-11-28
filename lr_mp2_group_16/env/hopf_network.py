ba98075db733eb67272f138aed2c08d931e98f02
Cyril955 <cyril.goffin@epfl.ch>
2025-11-28 11:34:52 +0100

    Best params for hisghest x velocity found empirically

--- lr_mp2_group_16/env/hopf_network.py
+++ lr_mp2_group_16/env/hopf_network.py
@@ -55,10 +55,10 @@ class HopfNetwork():
                 coupling_strength=1,     # coefficient to multiply coupling matrix
                 couple=True,             # whether oscillators should be coupled
                 time_step=0.001,         # time step 
-                ground_clearance=0.07,   # foot swing height 
-                ground_penetration=0.01, # foot stance penetration into ground 
+                ground_clearance=0.04,   # foot swing height 
+                ground_penetration=0.02, # foot stance penetration into ground 
                 robot_height=0.3,        # in nominal case (standing) 
-                des_step_len=0.05,       # desired step length 
+                des_step_len=0.07,       # desired step length 
                 max_step_len_rl=0.1,     # max step length, for RL scaling 
                 use_RL=False             # whether to learn parameters with RL 
                 ):
